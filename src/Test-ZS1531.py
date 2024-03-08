0613a29ed66f6dd93c308b06b00b946800a8a3bb
8K065 <146297908+8K065@users.noreply.github.com>
2024-03-07 21:46:48 -0600

    Add files via upload

--- src/Test-ZS1531.py
+++ src/Test-ZS1531.py
@@ -16,25 +16,92 @@ parquet_file = pq.ParquetFile('OptionMetrics.parquet')
 df = parquet_file.read().to_pandas()
 
 #print(df.head())
+filter=[]
+step=[]
+remaining=[]
+deleted=[]
 
+
+filter.append('Starting')
+step.append('Calls')
+remaining.append(1704128)
+deleted.append(' ')
+
+filter.append(' ')
+step.append('Puts')
+remaining.append(1706268)
+deleted.append(' ')
+
+filter.append(' ')
+step.append('All')
+remaining.append(len(df))
+deleted.append(' ')
 
 df=filter_level1.clean_options_data(df)
+remaining.append(len(df))
+deleted.append(remaining[2]-len(df))
+filter.append('Level 1 filters')
+step.append('clean_options_data')
 
 df=filter_level1.remove_duplicate_quotes(df)
+remaining.append(len(df))
+deleted.append(remaining[3]-len(df))
+filter.append(' ')
+step.append('remove_duplicate_quotes')
 
 df=filter_level1.delete_zero_bid_filter(df)
+remaining.append(len(df))
+deleted.append(remaining[4]-len(df))
+filter.append(' ')
+step.append('delete_zero_bid_filter')
 
 df=filter_andy.DaystoMaturity_filter(df)
+remaining.append(len(df))
+deleted.append(remaining[5]-len(df))
+filter.append('Level 2 filters')
+step.append('DaystoMaturity_filter')
 
 df=filter_andy.ExtremeIV_filter(df)
+remaining.append(len(df))
+deleted.append(remaining[6]-len(df))
+filter.append(' ')
+step.append('ExtremeIV_filter')
 
 df=filter_2_3_ZS.moneyness_filter(df)
+remaining.append(len(df))
+deleted.append(remaining[7]-len(df))
+filter.append(' ')
+step.append('moneyness(0.8~1.2)_filter')
 
 df=filter_2_3_ZS.implied_interest_rate_filter(df)
+remaining.append(len(df))
+deleted.append(remaining[8]-len(df))
+filter.append(' ')
+step.append('implied_interest_rate_filter')
 
 df=filter_2_3_ZS.IV_filter(df)
+remaining.append(len(df))
+deleted.append(remaining[9]-len(df))
+filter.append('Level 3 filters')
+step.append('IV_filter')
 
 df=filter_2_3_ZS.parity_filter(df)
+remaining.append(len(df))
+deleted.append(remaining[10]-len(df))
+filter.append(' ')
+step.append('put_call_parity_filter')
 
-#print(df)
-df.to_parquet('./data/filtered_for_HY.parquet')
+#print(remaining)
+#print(deleted)
+
+summary = pd.DataFrame({
+    'Level': step,  # 假设 step 是包含"Level"列数据的序列
+    'Filter': filter,  # filter 是"Filter"列的数据
+    'Deleted': deleted,  # deleted 是"Deleted"列的数据
+    'Remaining': remaining  # remaining 是"Remaining"列的数据
+}).set_index('Filter')
+
+print(summary)
+df.to_parquet('./data/data_filter_3.parquet')
+
+summary.to_excel('./output/latex.xlsx')
